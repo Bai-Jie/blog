7de18f8afb8d2de112837cd26516526cf137044b
Bai Jie <baijie1991@gmail.com>
2016-09-25 18:31:32 +0800

    ng g class blog

---
+++ code/client/web/src/app/shared/blog.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {Blog} from './blog';
+
+describe('Blog', () => {
+  it('should create an instance', () => {
+    expect(new Blog()).toBeTruthy();
+  });
+});
