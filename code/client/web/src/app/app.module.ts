6e5cdd092e207833584a30e66735b3fe3bd5b1e3
Bai Jie <baijie1991@gmail.com>
2016-09-25 18:26:05 +0800

    ng g component blog-list

--- code/client/web/src/app/app.module.ts
+++ code/client/web/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogListComponent
   ],
   imports: [
     BrowserModule,
