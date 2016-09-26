b34e33c3be0ae5008046436e3df30eda17faee46
Bai Jie <baijie1991@gmail.com>
2016-09-26 12:12:31 +0800

    set style
    change: use Bootstrap 3
    change: AppComponent.title

--- code/client/web/src/app/shared/dummy-blogs.ts
+++ code/client/web/src/app/shared/dummy-blogs.ts
@@ -4,6 +4,6 @@ export const BLOGS: Blog[] = [];
 
 let sampleParagraph = `Angular is built by a team of engineers who share a passion for making web development feel effortless. We believe that writing beautiful apps should be joyful and fun. We're building a platform for the future.`
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i <= 10; i++) {
   BLOGS.push({id: i.toString(), title: `blog${i}`, content: `sample paragraph ${i}. ${sampleParagraph}`});
 }
